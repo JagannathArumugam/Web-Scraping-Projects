1d651716f5cbbee59d0aca4c9acdb8c8a26d1cb3
Jagannath Arumugam <45625449+JagannathArumugam@users.noreply.github.com>
2022-12-17 22:07:57 +0530

    minor code changes

--- Yahoo-Finance-Scraper/main.py
+++ Yahoo-Finance-Scraper/main.py
@@ -11,9 +11,6 @@ def main():
     # Ticker symbols
     stocks = ['META', 'AAPL', 'AMZN', 'NFLX', 'GOOG']
 
-    stock_page = ''
-    stock_price = 0
-
     response = req.get(base_url)
     print(f'[-] Status code: {response.status_code}')
 
@@ -22,16 +19,13 @@ def main():
         stock_page = req.get(full_url)
         if stock_page.status_code == 200:
             page_source = stock_page.text
-
-            # Price element
             parsed_page = soup(page_source, 'html.parser')
             try:
-                stock_price = parsed_page.find('fin-streamer', attrs={'data-symbol': f'{ticker}'}, class_="Fw(b) Fz(36px) Mb(-4px) D(ib)").text
+                # Price element
+                stock_price = parsed_page.find('fin-streamer', attrs={'data-symbol': f'{ticker}'}).text
+                print(f'[-] Ticker {ticker} = {stock_price}')
             except:
-                pass
-
-            print(f'[-] Ticker {ticker} = {stock_price}')
-
+                print('Error. Cannot Find the Price Element.')
         else:
             print(f'[x] Request to the page {full_url} failed.\n[x] Status code: {stock_page.status_code}')
         time.sleep(2)
